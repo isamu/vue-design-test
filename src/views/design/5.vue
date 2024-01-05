755e5785c3a312c106d3846adf5ed293c99ca522
isamu <isamu@to-kyo.to>
2024-01-05 17:12:14 +0900

    update

--- src/views/design/5.vue
+++ src/views/design/5.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="h-[500vh]">
-    <div ref="main" class="sticky top-0 h-[100vh]">
+  <div class="h-[500vh]" ref="main" id="main">
+    <div class="sticky top-0 h-[100vh]" ref="page1">
       page 1
-      {{ top }}
+      <img src="/a.jpg" :style="style" />
+      {{ scrollTopPosition }}
     </div>
     <div class="sticky top-0 bg-black h-[100vh]">
       page 2
-      {{ top }}
+      {{ scrollTopPosition }}
     </div>
 
     <div class="sticky top-0 bg-pink-400 h-[100vh]">
       page 3
-      {{ top }}
+      {{ scrollTopPosition }}
     </div>
     <div class="bg-white relative h-[100vh]">
       page 4
-      {{ top }}
+      {{ scrollTopPosition }}
     </div>
     <div class="bg-white relative h-[100vh]">
       page 5
-      {{ top }}
+      {{ scrollTopPosition }}
     </div>
   </div>
 </template>
@@ -30,12 +31,25 @@ export default defineComponent({
   components: {},
   setup() {
     const main = ref();
-    const top = ref(0);
+    const page1 = ref();
+    const style = ref({});
+
+    const scrollTopPosition = ref(0);
+    const currentPage = ref(0);
     const showScrollTop = () => {
-      console.log("AA");
-      console.log(window.scrollY);
-      console.log(main.value);
-      top.value = window.scrollY;
+      // console.log("AA");
+      // console.log(main.value.offsetTop);
+      // const topOffset = window.scrollY + main.value.getBoundingClientRect().top;
+      // const currentScrollPosition = window.scrollY;
+      const pageHeight = page1.value.getBoundingClientRect().height;
+      const pageTop = -main.value.getBoundingClientRect().top;
+      currentPage.value = Math.floor(pageTop / pageHeight);
+
+      const posInPage = Math.floor(pageTop % pageHeight);
+      const pageRatio = Math.floor((100 * posInPage) / pageHeight);
+      console.log(pageTop, pageHeight, currentPage.value, pageRatio);
+
+      scrollTopPosition.value = window.scrollY;
     };
 
     onMounted(() => {
@@ -44,7 +58,9 @@ export default defineComponent({
 
     return {
       main,
-      top,
+      scrollTopPosition,
+      page1,
+      style,
     };
   },
 });
